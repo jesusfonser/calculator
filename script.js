ead223b9151a05fb5f60df1488addc07ef312a60
jesusfonser <jesusfonseruma@gmail.com>
2024-09-17 20:28:15 +0200

    Implementa las funciones de las operaciones básicas

---
+++ script.js
@@ -0,0 +1,19 @@
+let num1
+let num2
+let sign
+
+function add(a, b){
+    return a + b
+}
+
+function subtract(a, b){
+    return a - b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    return a / b
+}
