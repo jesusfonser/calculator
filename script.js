b2c2416694161d2dbb81f2e000e5ededab4e0226
jesusfonser <jesusfonseruma@gmail.com>
2024-09-17 20:50:45 +0200

    Comienza a construir html y pon botones

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ function multiply(a, b){
 function divide(a, b){
     return a / b
 }
+
+function operate (operator, num1, num2){
+    switch (operator){
+        case "+":
+            return add(num1, num2)
+            break;
+        case "-":
+            return subtract(num1, num2)
+            break;
+        case "*":
+            return multiply(num1, num2)
+            break;
+        case "/":
+            return divide(num1, num2)
+            break;
+    }
+}
