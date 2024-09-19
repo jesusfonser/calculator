c4dd1d9920df9085e331a6d0e410713902754b39
jesusfonser <jesusfonseruma@gmail.com>
2024-09-20 00:05:41 +0200

    Plantea funciones para los botones de signo

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let num1
 let num2
 let sign
+let resultado
+const operacion = {}
 let numerosEscritos = document.querySelector("#numeros")
 
 function add(a, b){
@@ -36,15 +38,29 @@ function operate (operator, num1, num2){
     }
 }
 
-
-console.log("Buenas")
 function escribirNum(a){
     if (numerosEscritos.textContent.length == 33) numerosEscritos.textContent = numerosEscritos.textContent.slice(1)
     numerosEscritos.textContent = numerosEscritos.textContent + a.textContent
     console.log("que tal")
 }
 
+function comienzaOperacion(a){
+    operacion.primero = numerosEscritos.textContent
+    operacion.signo = a.textContent
+    numerosEscritos.textContent = '';
+}
+
+function ejecutaOperacion(){
+    operacion.primero.includes(".") ? num1 = parseFloat(operacion.primero) : num1 = parseInt(operacion.primero)
+    numerosEscritos.textContent.includes(".") ? num2 = parseFloat(numerosEscritos.textContent) : num2 = parseInt(numerosEscritos.textContent)
+    resultado = Math.round(operate(operacion.signo, num1, num2))
+    numerosEscritos.textContent = resulado.toString().slice(0, 33)
+}
+
 let allBNum = document.querySelectorAll(".numero");
 for (let i = 0; i < allBNum.length; i++){
     allBNum[i].addEventListener("click", () => escribirNum(allBNum[i]))
 }
+
+
+
